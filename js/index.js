ab24fc1a58af8a2af86c16ce815a99a84eaba823
Santiago Chaves <santiagochaves242@gmail.com>
2022-04-22 22:27:11 -0300

    Blog

--- js/index.js
+++ js/index.js
@@ -1,14 +1,10 @@
-var swiper = new Swiper(".mySwiper", {
-  effect: "coverflow",
-  grabCursor: true,
-  centeredSlides: true,
-  slidesPerView: "auto",
-  coverflowEffect: {
-    rotate: 0,
-    stretch: 0,
-    depth: 100,
-    modifier: 2,
-    slideShadows: false,
-  },
-  loop: true
-});
+// Back to top
+const btn = document.querySelector(".back-to-top");
+window.onscroll = () => scrollFunction();
+scrollFunction();
+
+function scrollFunction() {
+  (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250)
+    ? btn.style.display = "flex"
+    : btn.style.display = "none";
+}
