8e78777dbeef5874e4bb875dee003ab54bf65621
Santiago Chaves <santiagochaves242@gmail.com>
2022-04-27 21:45:13 -0300

    Prueba

--- js/index.js
+++ js/index.js
@@ -33,7 +33,12 @@ const modalTitle = document.querySelector('.modal-title');
 const modalBody = document.querySelector('.modal-body');
 
 async function getEvento(id) {
-  const todo = await (await fetch('../eventos.json')).json();
+  const todo = await (await fetch('../eventos.json', {
+    'mode': 'cors',
+    'headers': {
+      'Access-Control-Allow-Origin': '*',
+    }
+  })).json();
   return todo.eventos.find(e => e.id == id);
 }
 
