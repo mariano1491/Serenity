af3c032bd3122361639f18c71776ec4ca27c7a22
Santiago Chaves <santiagochaves242@gmail.com>
2022-04-28 21:25:08 -0300

    Arreglos Generales

--- js/backToTop.js
+++ js/backToTop.js
@@ -1,3 +1,13 @@
+// Nav
+// const navbar = document.querySelector(".ftco-section");
+// const topDist = 36.794;
+
+// window.addEventListener('scroll', () => {
+//   (document.documentElement.scrollTop || document.body.scrollTop) >= topDist
+//     ? navbar.classList.add("sticky")
+//     : navbar.classList.remove("sticky");
+// });
+
 // Back to top
 const btn = document.querySelector(".back-to-top");
 window.onscroll = () => scrollFunction();
